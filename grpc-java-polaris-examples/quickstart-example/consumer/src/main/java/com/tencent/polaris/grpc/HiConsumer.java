af656b8df6301fc06dc8832ef6de4bf27ce843b2
lixiaoshuang <644968328@qq.com>
2021-12-17 16:49:58 +0800

    Resolver 添加默认命名空间

--- grpc-java-polaris-examples/quickstart-example/consumer/src/main/java/com/tencent/polaris/grpc/HiConsumer.java
+++ grpc-java-polaris-examples/quickstart-example/consumer/src/main/java/com/tencent/polaris/grpc/HiConsumer.java
@@ -31,7 +31,7 @@ public class HiConsumer {
     public static void main(String[] args) {
         
         NameResolverRegistry.getDefaultRegistry().register(new PolarisNameResolverProvider());
-        ManagedChannel channel = ManagedChannelBuilder.forTarget("polaris://Hi?namespace=default").usePlaintext()
+        ManagedChannel channel = ManagedChannelBuilder.forTarget("polaris://grpc-demo-java").usePlaintext()
                 .build();
         
         HiGrpc.HiBlockingStub hiBlockingStub = HiGrpc.newBlockingStub(channel);
